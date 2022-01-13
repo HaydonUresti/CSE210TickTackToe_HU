75bce8d3e9180120607dd9c153f4d4fc2101e4af
HaydonUresti <97619164+HaydonUresti@users.noreply.github.com>
2022-01-13 12:56:21 -0700

    Update TickTackToe.py

--- TickTackToe.py
+++ TickTackToe.py
@@ -1,9 +1,17 @@
-space_list = ["1", "2", "3", "4", "5", "6," "7", "8", "9" ]
+
 
 
 
 def board(spaces):
-    print("1")
+    print(f"{spaces[0]}|{spaces[1]}|{spaces[2]} \n-----\n{spaces[3]}|{spaces[4]}|{spaces[5]} \n-----\n{spaces[6]}|{spaces[7]}|{spaces[8]} ")
 
 
 
+def main(): 
+    space_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+
+    while True:
+        board(space_list)
+
+if __name__ == "__main__":
+    main()
