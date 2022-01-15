411bbd072075b31d1083ea97db0e9a718fa18242
HaydonUresti <97619164+HaydonUresti@users.noreply.github.com>
2022-01-15 11:25:35 -0700

    Update TickTackToe.py
    Added function to allow quitting whenever input is taken.

--- TickTackToe.py
+++ TickTackToe.py
@@ -7,7 +7,7 @@ def board(spaces):
 
 
 def make_choice(spaces, player):
-    """A function that takes user input and adds it to the board by appending the list containing each space's contents
+    """A function that takes and tests user input and adds it to the board by appending the list containing each space's contents
     Parameters
         spaces: takes a list that holds the value of the current space
         player: the current value to be placed on the board (X/O)
@@ -15,6 +15,10 @@ def make_choice(spaces, player):
     while True:
 
         choice = input("Enter the number of the space you want to mark (1-9): ")
+        if choice == 'q':
+            exit_message()
+        
+        # tests input for board availability
         if choice not in spaces:
             print("\nThat is not an available space. Please enter an available space\n")
         else:
@@ -23,7 +27,7 @@ def make_choice(spaces, player):
     for i in range(len(spaces)):
         if spaces[i] == choice:
             spaces[i] = player
-    
+
 def test_horizontal(spaces):
     """A function that checks for a winner on the x axis
     Parameters
@@ -64,11 +68,24 @@ def test_diagonal(spaces):
     else:
         return False
 
-
+def exit_message():
+    """A function that gives the user the option to quit the game"""
+    while True:
+        quit = input("Are you sure you want to quit? Please enter 'y' or 'n': ")
+        if quit == 'y':
+            exit()
+        elif quit == 'n':
+            break
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
 def main(): 
     space_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9" ]
     turn = 1
     
+    print("Welcome to Tick-Tack-Toe!")
+    answer = input("Please press enter to continue or type 'q' at any time to quit: ")
+    if answer == 'q':
+        exit_message()
     # Main game loop
     while True:
 
@@ -81,13 +98,16 @@ def main():
         board(space_list)
 
         make_choice(space_list, player)
-
+        
+        #checking for a winner
         if test_horizontal(space_list) or test_vertical(space_list) or test_diagonal(space_list):
             print(f"\n{player} Wins!!")
             break
 
 
         turn += 1
+
+        #checks for a draw
         if turn == 10:
             print("DRAW, GAME OVER...")
 
